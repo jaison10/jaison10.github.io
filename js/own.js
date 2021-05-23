8a52862df91903ab1a73aaf6496542d0012c733c
jaison10 <jaisonj1010@gmail.com>
2021-05-23 18:43:09 +0530

    designation forward and backward

--- js/own.js
+++ js/own.js
@@ -1,17 +1,61 @@
 var desigs = ["Student","Web developer","Programmer","UI designer","Researcher","Video editor"];
 
-window.addEventListener('DOMContentLoaded', (event) => {
+//         MOVING CONTENT BACKWARDS - Removing.
+
+// window.addEventListener('DOMContentLoaded', (event) => {
+//     console.log('DOM fully loaded and parsed');
+//     let i = 0;
+//     document.getElementById("designation").innerText = desigs[i];
+
+//     setInterval(() => {
+//         document.getElementById("designation").innerText = desigs[i];
+//         i++;
+//         if(i == 6){
+//             i = 0;
+//         }
+        
+//         let current = document.getElementById("designation").textContent;
+//             let len = current.length;
+//             // console.log(current);
+//             // console.log(len);
+//             let time = Math.floor(2000/len);
+//             // console.log(time);
+//             var count = 1;
+//             var newCurrent = current;
+//             var inner = setInterval(() => {
+//                 newCurrent = newCurrent.substring(0, newCurrent.length - 1);
+//                 count++;
+//                 // console.log(newCurrent);
+//                 if(count == current.length){
+//                     clearInterval(inner);
+//                 }
+//                 document.getElementById("designation").innerText = newCurrent;
+//             }, time);
+//             // document.getElementById("designation").innerText = newCurrent;
+//     }, 2000);
+// });
+
+
+// CONTENT ADDED - Forward.
+
+window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
     let i = 0;
-    // while(i<4){
-        document.getElementById("designation").innerText = desigs[i];
-    //     i++;
-    //     if(i == 4){
-    //         i = 0;
-    //     }
-    // }
+    document.getElementById("designation").innerText = "Welcome!"
     setInterval(() => {
-        document.getElementById("designation").innerText = desigs[i];
+        let current = desigs[i];
+        let len = current.length;
+        let time = Math.floor(2000/len);
+        var count = 1;
+        var inner = setInterval(() => {
+            newCurrent = current.substring(0, count);
+            count++;
+            if(count == current.length+1){
+                clearInterval(inner);
+            }
+            document.getElementById("designation").innerText = newCurrent;
+        }, time);
+
         i++;
         if(i == 6){
             i = 0;
